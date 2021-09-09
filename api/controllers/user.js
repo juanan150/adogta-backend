1118c77b62bbd36440a7931d6034cd9690bdc6d4
John <cortesjohnj@gmail.com>
2021-09-09 16:47:10 -0500

    start moving folders

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,17 +1,34 @@
 const User = require("../models/User");
+const Foundation = require("../models/Foundation");
 
 const createUser = async (req, res, next) => {
-  try {
-    const newUser = await new User(req.body);
-    await newUser.save();
-    res.status(201).json(newUser);
-  } catch (err) {
-    if (err.name === "ValidationError") {
-      console.log("Error de validación:", err.errors);
-      res.status(422).json(err.message);
-    } else {
-      next(err);
-      console.log(err);
+  if (req.body.role === "user") {
+    try {
+      const newUser = await new User(req.body);
+      await newUser.save();
+      res.status(201).json(newUser);
+    } catch (err) {
+      if (err.name === "ValidationError") {
+        console.log("Error de validación:", err.errors);
+        res.status(422).json(err.message);
+      } else {
+        next(err);
+        console.log(err);
+      }
+    }
+  } else {
+    try {
+      const newFoundation = await new Foundation(req.body);
+      await newFoundation.save();
+      res.status(201).json(newFoundation);
+    } catch (err) {
+      if (err.name === "ValidationError") {
+        console.log("Error de validación:", err.errors);
+        res.status(422).json(err.message);
+      } else {
+        next(err);
+        console.log(err);
+      }
     }
   }
 };
---
+++ api/routers/foundation.js
@@ -0,0 +1,7 @@
+// const express = require("express");
+// const app = express.Router();
+// const createFoundation = require("../controllers/foundation");
+
+// const foundationApp = app.post("/signup2", createFoundation);
+
+// module.exports = foundationApp;
