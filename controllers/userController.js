081cbcb8d788bec9a056b7c8c382c704ca827445
Diego Plata <diegooplata@gmail.com>
2021-09-09 14:11:13 -0500

    Fix user login functionalities

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,13 +4,19 @@ var jwt = require("jsonwebtoken");
 const login = async (req, res) => {
   const { email, password } = req.body;
 
-  const user = await User.authenticate(email, password);
+  let user = await User.authenticate(email, password);
 
   if (user) {
     const token = jwt.sign({ userId: user._id }, "secret key");
-    res.json({ token });
+    res.json({ token, user });
   } else {
-    res.status(401).json({ error: "Credenciales inválidas" });
+    user = await Foundation.authenticate(email, password);
+    if (user) {
+      const token = jwt.sign({ userId: user._id }, "secret key");
+      res.json({ token, user });
+    } else {
+      res.status(401).json({ error: "Credenciales inválidas" });
+    }
   }
 };
 
