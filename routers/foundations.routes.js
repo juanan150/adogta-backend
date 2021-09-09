e92892b20a0190fe5cd4659a9c041e66c8ad21fb
Diego Plata <diegooplata@gmail.com>
2021-09-09 14:14:39 -0500

    Remove unnecessary files

--- routers/foundations.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const login = require("../controllers/foundationController");
-
-const router = express.Router();
-
-router.post("/login", login);
-
-module.exports = router;
