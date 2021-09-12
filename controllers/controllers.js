3867a3b10ca121ad16b02d582c2281027ba8ccf3
John <cortesjohnj@gmail.com>
2021-09-12 12:47:40 -0500

    change error management and added an optional route to get user info

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -2,6 +2,7 @@ const User = require("../models/User");
 const Foundation = require("../models/Foundation");
 
 const createUser = async (req, res, next) => {
+  console.log(req.body.role);
   if (req.body.role === "user") {
     try {
       const newUser = await new User(req.body);
@@ -9,22 +10,24 @@ const createUser = async (req, res, next) => {
       res.status(201).json(newUser);
     } catch (err) {
       if (err.name === "ValidationError") {
-        console.log("Error de validación:", err.errors);
-        res.status(422).json(err.message);
+        console.log("Validation Error:", err.errors);
+        res.status(422).json(err.errors);
       } else {
         next(err);
         console.log(err);
       }
     }
-  } else {
+  } else if (req.body.role === "foundation") {
+    console.log(req.body.role);
     try {
       const newFoundation = await new Foundation(req.body);
       await newFoundation.save();
       res.status(201).json(newFoundation);
     } catch (err) {
       if (err.name === "ValidationError") {
-        console.log("Error de validación:", err.errors);
-        res.status(422).json(err.message);
+        console.log("Validation Error:", err.errors);
+        res.status(422).json(err.errors);
+        console.log(err);
       } else {
         next(err);
         console.log(err);
@@ -33,4 +36,21 @@ const createUser = async (req, res, next) => {
   }
 };
 
-module.exports = createUser;
+const getUser = async (req, res, next) => {
+  console.log(req.body.email);
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    console.log(user);
+    res.status(201).json(user);
+  } catch (err) {
+    if (err.name === "ValidationError") {
+      console.log("Validation Error:", err.errors);
+      res.status(422).json(err.errors);
+    } else {
+      next(err);
+      console.log(err);
+    }
+  }
+};
+
+module.exports = { createUser, getUser };
--- routers/routes.js
+++ routers/routes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express.Router();
-const createUser = require("../controllers/controllers");
+const { auth } = require("../middlewares/middelwares");
+const { createUser, getUser } = require("../controllers/controllers");
 
 app.post("/signup", createUser);
-
+app.get("/", auth, getUser); // It's optional, must change the url to users id
 module.exports = app;
