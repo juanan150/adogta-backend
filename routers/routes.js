75df62e7de9d89d290d25fb2529cc76822e44caf
Diego Plata <diegooplata@gmail.com>
2021-09-15 20:10:47 -0500

    Add user profile routes

--- routers/routes.js
+++ routers/routes.js
@@ -5,6 +5,8 @@ const app = express.Router();
 
 app.get("/me", auth, controllers.loadUser);
 app.post("/login", controllers.login);
+app.get("/:id/profile", auth);
+app.put("/:id/profile", auth, controllers.updateProfile);
 app.delete("/pets/:id", auth, controllers.destroyPet);
 app.get("/foundations/:id/pets", auth, controllers.listPets);
 app.post("/foundations/:foundationId/pets", auth, controllers.createPet);
