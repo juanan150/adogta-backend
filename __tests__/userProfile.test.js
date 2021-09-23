7e7884774722b561a6ad041b51624320adfeee86
Diego Plata <diegooplata@gmail.com>
2021-09-22 20:48:35 -0500

    Create more user profile tests

--- __tests__/userProfile.test.js
+++ __tests__/userProfile.test.js
@@ -24,6 +24,7 @@ describe("put/:id/profile", () => {
         name: faker.name.findName(),
         role: "user",
         address: faker.address.city(),
+        phoneNumber: faker.phone.phoneNumber(),
       })
       .set("Authorization", token);
 
@@ -37,8 +38,49 @@ describe("put/:id/profile", () => {
       name: faker.name.findName(),
       role: "user",
       address: faker.address.city(),
+      phoneNumber: faker.phone.phoneNumber(),
     });
 
     expect(response.statusCode).toBe(401);
+  });
+
+  test("Response with correct json", async () => {
+    const response = await request(app)
+      .put(`/${_id}/profile`)
+      .send({
+        _id: _id,
+        email: "foundationt@test.com",
+        name: faker.name.findName(),
+        role: "user",
+        address: faker.address.city(),
+        phoneNumber: faker.phone.phoneNumber(),
+      })
+      .set("Authorization", token);
+    expect(response.type).toBe("application/json");
+  });
+
+  test("Response with correct properties", async () => {
+    const response = await request(app)
+      .put(`/${_id}/profile`)
+      .send({
+        _id: _id,
+        email: "foundationt@test.com",
+        name: faker.name.findName(),
+        role: "user",
+        address: faker.address.city(),
+        phoneNumber: faker.phone.phoneNumber(),
+        photoUrl: faker.image.imageUrl(),
+      })
+      .set("Authorization", token);
+
+    expect(response.body).toEqual({
+      email: expect.any(String),
+      address: expect.any(String),
+      phoneNumber: expect.any(String),
+      role: expect.any(String),
+      name: expect.any(String),
+      photoUrl: expect.any(String),
+      _id: expect.any(String),
+    });
   });
 });
