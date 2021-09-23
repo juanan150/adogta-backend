4f7aba87d362150c5415ab40efeba83a2d9018ed
John <cortesjohnj@gmail.com>
2021-09-23 17:38:02 -0500

    Create project, start testing user Register, finish tests

---
+++ app.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const config = require("./config");
+const routes = require("./routers/routes");
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+process.env.NODE_ENV === "test"
+  ? mongoose.connect(
+      config.dbConnectionStringTest,
+      console.log("Connected to db-test")
+    )
+  : mongoose.connect(config.dbConnectionString, console.log("Connected to db"));
+
+mongoose.connection.on("error", function (e) {
+  console.error(e);
+});
+
+//routes that will be used
+app.use(routes);
+
+//manage errors
+app.use((err, req, res, next) => {
+  res.status(500).json({ error: err.message });
+});
+
+module.exports = app;
