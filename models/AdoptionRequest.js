31dfedfdfad792e0ef0664b823da155130b6aa47
Juan <juanan0309@gmail.com>
2021-09-29 14:09:18 -0500

    Add timestamps to models

--- models/AdoptionRequest.js
+++ models/AdoptionRequest.js
@@ -1,25 +1,30 @@
 const mongoose = require("mongoose");
 
-const adoptionRequestSchema = mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
+const adoptionRequestSchema = mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    petId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Pet",
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    responseStatus: {
+      default: "pending",
+      type: String,
+    },
   },
-  petId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Pet",
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  responseStatus: {
-    default: "pending",
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const AdoptionRequest = mongoose.model(
   "adoptionrequests",
--- models/Pet.js
+++ models/Pet.js
@@ -1,33 +1,38 @@
 const mongoose = require("mongoose");
 
-const petSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Name is required"],
+const petSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
+    description: {
+      type: String,
+      required: [true, "A description is required"],
+    },
+    photoUrl: {
+      //Pendiente con cloudinary
+      type: Array,
+      required: [false, "A description is required"],
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    foundationId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Foundation",
+      required: true,
+    },
+    adopted: {
+      type: Boolean,
+      default: false,
+    },
   },
-  description: {
-    type: String,
-    required: [true, "A description is required"],
-  },
-  photoUrl: {
-    //Pendiente con cloudinary
-    type: Array,
-    required: [false, "A description is required"],
-  },
-  age: {
-    type: Number,
-    required: true,
-  },
-  foundationId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Foundation",
-    required: true,
-  },
-  adopted: {
-    type: Boolean,
-    default: false,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Pet = mongoose.model("Pet", petSchema);
 
